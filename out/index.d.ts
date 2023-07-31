222b45a5c96db2eed8f53c3ce63d80d6c79d60bd
DerBot7214 <lengwenath1@gmail.com>
2023-07-31 02:37:50 +0200

    rename package

--- out/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { NodeSetupConfig, PackageJson } from './types';
-declare const defaultConfig: NodeSetupConfig;
-declare function setupEnv(): Promise<void>;
-export { NodeSetupConfig, PackageJson, defaultConfig, setupEnv };
